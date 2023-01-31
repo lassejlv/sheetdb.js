fd98ed8c0d99e35ad81d16186ce70bbe768280b6
lassev05 <vestergaardlasse2@gmail.com>
2023-02-01 00:07:37 +0100

    delete, update Data

--- src/Client.js
+++ src/Client.js
@@ -14,6 +14,15 @@ class SheetClient {
     }
   }
 
+  // Use Logger
+  useLogger() {
+    if (this.options.useLogger) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   // Create Data
   async createData(data_value) {
     // Get the current time
@@ -36,7 +45,7 @@ class SheetClient {
     }
 
     // Make a request to the API
-    fetch(`${api_endpoint}/${this.options.connect}`, {
+    return fetch(`${api_endpoint}/${this.options.connect}`, {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -48,9 +57,98 @@ class SheetClient {
     })
       .then((response) => response.json())
       .then((data) => {
-        // Log the data
-        console.log("Created Data: ", data_value);
-        console.log("Time: ", Date.now() - Time, "ms");
+        if (this.useLogger()) {
+          // Log the data
+          console.log("Created Data: ", data_value);
+          console.log("Time: ", Date.now() - Time, "ms");
+        }
+
+        // Return the data
+        return data_value;
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  // Delete Data
+  async deleteData(data_id) {
+    // Get the current time
+    const Time = Date.now();
+    // Check user has provided a connection string
+    this.checkConnectionString();
+
+    // Make a request to the API
+    return fetch(`${api_endpoint}/${this.options.connect}/id/${data_id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (this.useLogger()) {
+          // Log the data
+          console.log("Deleted ID: ", data_id);
+          console.log("Time: ", Date.now() - Time, "ms");
+        }
+
+        // Return the data
+        if (data.error) {
+          throw new Error(data.error);
+        } else {
+          return data;
+        }
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  // Update Data
+  async updateData(data_id, data_value) {
+    // Get the current time
+    const Time = Date.now();
+    // Check user has provided a connection string
+    this.checkConnectionString();
+
+    // Check if the data is an array
+    if (Array.isArray(data_value)) {
+      throw new Error("Data must be an object");
+    }
+
+    // If array must contian an id property
+    if (data_id[0].id) {
+      throw new Error(
+        "You can't update the id property inside a update request"
+      );
+    }
+
+    // Make a request to the API
+    return fetch(`${api_endpoint}/${this.options.connect}/id/${data_id}`, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data: [data_value],
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (this.useLogger()) {
+          // Log the data
+          console.log("Updated Data: ", data_value);
+          console.log("Time: ", Date.now() - Time, "ms");
+        }
+
+        if (data.error) {
+          throw new Error(data.error);
+        } else {
+          return data;
+        }
       })
       .catch((error) => {
         throw new Error(error);
--- test/index.js
+++ test/index.js
@@ -1,8 +1,61 @@
 const { SheetClient } = require("../index");
+const prompt = require("prompt-sync")();
 
 const sheetDbClient = new SheetClient({
   // Enter ConnectionString APP_ID
   connect: "mfpis1bnqy29i",
+  useLogger: false,
 });
 
-sheetDbClient.createData([{ id: "INCREMENT", name: "username", age: 12 }]);
+// // Simple Project Using SheetDB
+// Create Data
+// sheetDbClient.createData([
+//   {
+//     id: "INCREMENT",
+//     name: "SheetDB",
+//   }]),
+
+// Delete Data
+// sheetDbClient.deleteData("1")
+
+// Update Data
+// sheetDbClient.updateData("1", { name: "Lasse Is Eine Cool" });
+
+// const name = prompt("Enter your name: ");
+// const age = prompt("Enter your age: ");
+
+// if (!name) {
+//   console.log("Please enter a name");
+// } else if (!age) {
+//   console.log("Please enter an age");
+// } else {
+//   sheetDbClient
+//     .createData([
+//       {
+//         id: "INCREMENT",
+//         name: name,
+//         age: age,
+//       },
+//     ])
+//     .then((db) => {
+//       console.log("You have been added to the database.");
+//     });
+// }
+
+const deleteUserId = prompt("Enter the id of the user you want to delete: ");
+
+if (!deleteUserId) {
+  console.log("Please enter a valid id");
+} else {
+  sheetDbClient
+    .deleteData(deleteUserId)
+    .then((db) => {
+      console.log("You have been deleted from the database.");
+    })
+    .catch((err) => {
+      console.log("There was an error deleting the user.");
+      setTimeout(() => {
+        console.log(err.message);
+      });
+    });
+}
